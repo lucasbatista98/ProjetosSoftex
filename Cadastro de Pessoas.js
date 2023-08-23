d811e1e208a8a6f83706f65d505f1fe6badbcc07
lucasbatista98 <110137649+lucasbatista98@users.noreply.github.com>
2023-08-23 13:52:32 -0300

    Add files via upload

---
+++ Cadastro de Pessoas.js
@@ -0,0 +1,19 @@
+// Definindo os tipos de variáveis
+const nome = "João"; // string
+const salario = 2500.5; // number
+const idade = 30; // number
+const possuiDiploma = true; // boolean
+
+// Exemplo de cadastro de pessoa
+const pessoa = {
+  nome: nome,
+  salario: salario,
+  idade: idade,
+  possuiDiploma: possuiDiploma,
+};
+
+// Exibindo os dados da pessoa cadastrada
+console.log("Nome: " + pessoa.nome); // Saída: Nome: João
+console.log("Salário: " + pessoa.salario); // Saída: Salário: 2500.5
+console.log("Idade: " + pessoa.idade); // Saída: Idade: 30
+console.log("Possui Diploma: " + pessoa.possuiDiploma); // Saída: Possui Diploma: true
